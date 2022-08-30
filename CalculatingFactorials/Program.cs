daaafa92e5a1bc7d181b252b8b03b77582821bc3
Sam Schultz <sam.schultz@outlook.com>
2022-08-30 11:25:34 -0400

    Add project files.

---
+++ CalculatingFactorials/Program.cs
@@ -0,0 +1,40 @@
+﻿
+/*      how I was trying to do it
+Factorial(4);
+
+void Factorial(int UserInput)
+{
+     for (int factorial = 1; factorial <= UserInput; factorial++)
+     { 
+        factorial *= UserInput;
+        Console.WriteLine(factorial);
+
+     }
+    
+}
+*/
+
+//How Greg did it
+int target = 4;
+int product = FactorialG(target);
+
+for(int idx = 1; idx <= target; idx++)
+{
+    product *= idx;
+}
+Console.WriteLine($"{target}! is {product}");
+
+
+
+int FactorialG(int nbr) 
+{ 
+    
+   if(nbr == 1)
+    {
+        return 1;
+    }
+    return nbr * FactorialG(nbr - 1);
+
+}                                                       //Need to make sure the function has a command to stop calling itself
+
+
